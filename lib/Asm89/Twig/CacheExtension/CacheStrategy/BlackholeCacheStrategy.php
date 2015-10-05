28379d035cacaa16daba829aa77d2d44322002e7
Hagen Hübel <hhuebel@itinance.com>
2015-10-05 14:43:32 +0200

    DocBlock added and header adjusted

--- lib/Asm89/Twig/CacheExtension/CacheStrategy/BlackholeCacheStrategy.php
+++ lib/Asm89/Twig/CacheExtension/CacheStrategy/BlackholeCacheStrategy.php
@@ -3,7 +3,7 @@
 /*
  * This file is part of twig-cache-extension.
  *
- * (c) Hagen Hübel <hhuebel@itinance.com>
+ * (c) Alexander <iam.asm89@gmail.com>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -13,9 +13,20 @@ namespace Asm89\Twig\CacheExtension\CacheStrategy;
 
 use Asm89\Twig\CacheExtension\CacheStrategyInterface;
 
+/**
+ * CacheStrategy which doesn't cache at all
+ *
+ * This strategy can be used in development mode, e.g. editing twig templates,
+ * to prevent previously cached versions from being rendered.
+ *
+ * @see     https://github.com/asm89/twig-cache-extension/pull/29
+ *
+ * @author  Hagen Hübel <hhuebel@itinance.com>
+ *
+ * @package Asm89\Twig\CacheExtension\CacheStrategy
+ */
 class BlackholeCacheStrategy implements CacheStrategyInterface
 {
-
     /**
      * {@inheritDoc}
      */
